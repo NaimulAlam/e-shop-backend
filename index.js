743b346f8ec452308fb6821902e105b09008ad0e
Mohmammad Naimul Alam <naimul2707@gmail.com>
2023-04-15 19:08:21 +0200

    database connaction fail resolved after code update, code refatored with async awit and more precise definition of constant naming

--- index.js
+++ index.js
@@ -1,129 +1,120 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
-const bodyParser = require("body-parser");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+// const bodyParser = require("body-parser");
 require("dotenv").config();
-const MongoClient = require("mongodb").MongoClient;
-const ObjectID = require("mongodb").ObjectID;
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v9g8ylp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("WellCome to EShopApi Server");
-});
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v9g8ylp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, {
+const mongo = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const ProductCollection = client.db("EShopApi Server").collection("products");
-  const userBasketCollection = client
-    .db("EShopApi Server")
-    .collection("userBasket");
-  const ReviewsCollection = client.db("EShopApi Server").collection("reviews");
-  const AdminCollection = client.db("EShopApi Server").collection("Admin");
 
-  app.get("/products", (req, res) => {
-    ProductCollection.find({}).toArray((err, items) => {
-      res.send(items);
-      if (err) {
-        res.send(err);
-      }
-    });
-  });
+async function run() {
+  try {
+    const ProductCollection = mongo
+      .db("eshopWsbDatabase")
+      .collection("products");
+    const userBasketCollection = mongo
+      .db("eshopWsbDatabase")
+      .collection("userBasket");
+    const ReviewsCollection = mongo
+      .db("eshopWsbDatabase")
+      .collection("reviews");
+    const AdminCollection = mongo.db("eshopWsbDatabase").collection("Admin");
 
-  app.get("/reviews", (req, res) => {
-    ReviewsCollection.find({}).toArray((err, items) => {
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const items = await ProductCollection.find(query).toArray();
       res.send(items);
     });
-  });
 
-  app.get("/allUserBasket", (req, res) => {
-    userBasketCollection.find({}).toArray((err, items) => {
-      res.send(items);
+    app.get("/reviews", async (req, res) => {
+      const query = {};
+      const reviews = await ReviewsCollection.find(query).toArray();
+      res.send(reviews);
     });
-  });
 
-  app.get("/products/:id", (req, res) => {
-    ProductCollection.find({ _id: ObjectID(req.params.id) }).toArray(
-      (err, documents) => {
-        res.send(documents[0]);
-      }
-    );
-  });
+    app.get("/allUserBasket", async (req, res) => {
+      const query = {};
+      const userBasket = await userBasketCollection.find(query).toArray();
+      res.send(userBasket);
+    });
 
-  app.post("/addProducts", (req, res) => {
-    const newProduct = req.body;
-    ProductCollection.insertOne(newProduct).then((result) => {
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await productCollection.findOne(filter);
+      res.send(result);
+    });
+
+    app.post("/addProducts", async (req, res) => {
+      const newProduct = req.body;
+      const result = await ProductCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
+    app.post("/addReview", async (req, res) => {
+      const newReview = req.body;
+      const result = ReviewsCollection.insertOne(newReview);
       res.send(result.insertedCount > 0);
     });
-  });
 
-  app.post("/addReview", (req, res) => {
-    const newReview = req.body;
-    ReviewsCollection.insertOne(newReview).then((result) => {
+    app.post("/addAdmin", async (req, res) => {
+      const newAdmin = req.body;
+      const result = await AdminCollection.insertOne(newAdmin);
       res.send(result.insertedCount > 0);
     });
-  });
 
-  app.post("/addAdmin", (req, res) => {
-    const newAdmin = req.body;
-    AdminCollection.insertOne(newAdmin).then((result) => {
+    app.post("/addToCart", async (req, res) => {
+      const newCart = req.body;
+      const result = await userBasketCollection.insertOne(newCart);
       res.send(result.insertedCount > 0);
     });
-  });
 
-  app.post("/addToCart", (req, res) => {
-    const newCart = req.body;
-    userBasketCollection.insertOne(newCart).then((result) => {
-      res.send(result.insertedCount > 0);
+    app.get("/userBasket", async (req, res) => {
+      const query = { email: req.query.email };
+      const useBasket = await userBasketCollection.find(query).toArray();
+      res.send(useBasket);
     });
-  });
 
-  app.get("/userBasket", (req, res) => {
-    userBasketCollection
-      .find({ email: req.query.email })
-      .toArray((err, documents) => {
-        if (err) {
-          res.send(err);
-        } else {
-          res.send(documents);
-        }
-      });
-  });
-
-  app.post("/isAdmin", (req, res) => {
-    AdminCollection.find({ email: req.body.email }).toArray((err, admin) => {
+    app.post("/isAdmin", async (req, res) => {
+      const query = { email: req.query.email };
+      const admin = AdminCollection.find(query).toArray();
       res.send(admin.length > 0);
     });
-  });
 
-  app.delete("/delete/:id", (req, res) => {
-    ProductCollection.deleteOne({ _id: ObjectID(req.params.id) }).then(
-      (result) => {
-        res.send(result.deletedCount > 0);
-      }
-    );
-  });
+    app.delete("/deleteProduct/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const deletedProduct = ProductCollection.deleteOne(filter);
+      res.send(deletedProduct);
+    });
 
-  app.patch("/updateBooking/:id", (req, res) => {
-    userBasketCollection
-      .updateOne(
-        { _id: ObjectID(req.params.id) },
-        {
-          $set: { status: req.body.status },
-        }
-      )
-      .then((result) => {
-        res.send(result.modifiedCount > 0);
-      });
-  });
+    app.patch("/updateProduct/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+
+      const updatedProduct = await userBasketCollection.updateOne(filter);
+      res.send(result.modifiedCount > 0);
+    });
+  } catch (err) {
+    console.log(err.stack);
+  }
+}
+
+run().catch(console.log);
+
+app.get("/", async (req, res) => {
+  res.send("WellCome to EShopApi Database Server");
 });
 
 app.listen(port, () => {
